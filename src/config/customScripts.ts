5ce47c1365088f1a75cadaa2e48ce01fcafd30d9
rabbykhairul <rabbykhairul@gmail.com>
2023-10-01 21:33:47 +0600

    extend the custom script config with additional types

--- src/config/customScripts.ts
+++ src/config/customScripts.ts
@@ -1,12 +1,24 @@
+export type ScriptConfig = {
+  displayName: string;
+  version: string;
+  path: string;
+};
+
 export const CUSTOM_SCRIPTS_BASE_FILE_PATH = "/src/server/webflow/scripts";
 
 export const CUSTOM_SCRIPTS_NAME = {
   TEST: "test script",
-};
+} as const;
 
-export const CUSTOM_SCRIPTS_CONFIG = {
+type scriptKeys = keyof typeof CUSTOM_SCRIPTS_NAME;
+type scriptValues = (typeof CUSTOM_SCRIPTS_NAME)[scriptKeys];
+
+export const CUSTOM_SCRIPTS_CONFIG: {
+  [key in scriptValues]: ScriptConfig;
+} = {
   [CUSTOM_SCRIPTS_NAME.TEST]: {
     version: "0.0.1",
     path: `${CUSTOM_SCRIPTS_BASE_FILE_PATH}/test.js`,
+    displayName: CUSTOM_SCRIPTS_NAME.TEST,
   },
 };
