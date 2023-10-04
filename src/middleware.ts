b2b3aefd9ed88913ada0f94dbcb0bb03854012b6
rabbykhairul <rabbykhairul@gmail.com>
2023-10-04 10:45:42 +0600

    enable cors in the api routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,33 @@ import { validateBasicAuth } from "./utils/auth";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
+  if (req.method === "OPTIONS") {
+    return new Response(null, {
+      headers: {
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT,OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+      },
+    });
+  }
+
   if (validateBasicAuth(req.headers)) {
-    return NextResponse.next();
+    const res = NextResponse.next();
+
+    res.headers.append("Access-Control-Allow-Credentials", "true");
+    res.headers.append("Access-Control-Allow-Origin", "*");
+    res.headers.append(
+      "Access-Control-Allow-Methods",
+      "GET,DELETE,PATCH,POST,PUT",
+    );
+    res.headers.append(
+      "Access-Control-Allow-Headers",
+      "Authorization, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+    );
+
+    return res;
   } else
     return Response.json(
       { message: "Invalid Authentication Credentials" },
