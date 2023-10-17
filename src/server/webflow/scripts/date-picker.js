878aedfe9b91911c0071c9db7c63a381ddcd863f
rabbykhairul <rabbykhairul@gmail.com>
2023-10-18 00:15:24 +0600

    date picker script, select all date pickers dynamically & make them interactive

--- src/server/webflow/scripts/date-picker.js
+++ src/server/webflow/scripts/date-picker.js
@@ -1,5 +1,19 @@
-const picker = new easepick.create({
-  element: document.getElementById("datepicker"),
-  css: ["https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css"],
-  zIndex: 10,
-});
+window.formFieldsDateInput = () => {
+  const selectors = {
+    DATE_PICKER: "[form-fields-pro-date-picker]",
+  };
+
+  const datePickers = document.querySelectorAll(selectors.DATE_PICKER);
+
+  for (let datePicker of datePickers) {
+    new easepick.create({
+      element: datePicker,
+      css: [
+        "https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css",
+      ],
+      zIndex: 10,
+    });
+  }
+};
+
+window.formFieldsDateInput();
