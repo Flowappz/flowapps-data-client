078821b2dbad3f7e3e5dc73df989836973459e51
rabbykhairul <rabbykhairul@gmail.com>
2023-10-17 21:07:44 +0600

    basic date picker script

--- src/server/webflow/scripts/date-picker.js
+++ src/server/webflow/scripts/date-picker.js
@@ -1,5 +1,5 @@
 const picker = new easepick.create({
-  element: "#datepicker",
+  element: document.getElementById("datepicker"),
   css: ["https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css"],
   zIndex: 10,
 });
