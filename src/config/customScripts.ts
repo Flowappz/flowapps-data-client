4f86028930652590b568bac293c02c4738ef577a
rabbykhairul <rabbykhairul@gmail.com>
2023-10-24 20:18:57 +0600

    update date picker script version to 0.0.3

--- src/config/customScripts.ts
+++ src/config/customScripts.ts
@@ -54,7 +54,7 @@ export const CUSTOM_SCRIPTS_CONFIG: {
 
   [CUSTOM_SCRIPTS_NAME.DATE_PICKER_SCRIPT]: {
     displayName: CUSTOM_SCRIPTS_NAME.DATE_PICKER_SCRIPT,
-    version: "0.0.2",
+    version: "0.0.3",
     location: "footer",
     path: `${CUSTOM_SCRIPTS_BASE_FILE_PATH}/minified/date-picker.min.js`,
   },
