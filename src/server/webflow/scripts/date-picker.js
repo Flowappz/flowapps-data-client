bb0fa420d8b794fffffab2f73c6a8c48fcd44007
rabbykhairul <rabbykhairul@gmail.com>
2023-10-24 20:15:44 +0600

    combine the date picker & date range picker script

--- src/server/webflow/scripts/date-picker.js
+++ src/server/webflow/scripts/date-picker.js
@@ -1,19 +1,42 @@
 window.formFieldsDateInput = () => {
+
   const selectors = {
     DATE_PICKER: "[form-fields-pro-date-picker]",
+    DATE_RANGE_PICKER: "[form-fields-pro-date-range-picker]",
   };
 
-  const datePickers = document.querySelectorAll(selectors.DATE_PICKER);
+  const initializeDatePickers = () => {
+    const datePickers = document.querySelectorAll(selectors.DATE_PICKER);
 
-  for (let datePicker of datePickers) {
-    new easepick.create({
-      element: datePicker,
-      css: [
-        "https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css",
-      ],
-      zIndex: 10,
-    });
-  }
+    for (let datePicker of datePickers) {
+      new easepick.create({
+        element: datePicker,
+        css: [
+          "https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css",
+        ],
+        zIndex: 10,
+      });
+    }
+  };
+
+  const initializeDateRangePickers = () => {
+    const datePickers = document.querySelectorAll(selectors.DATE_RANGE_PICKER);
+
+    for (let datePicker of datePickers) {
+      new easepick.create({
+        element: datePicker,
+        css: [
+          "https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css",
+        ],
+        zIndex: 10,
+        plugins: ["RangePlugin"],
+      });
+    }
+  };
+
+  initializeDatePickers();
+  initializeDateRangePickers();
+
 };
 
 window.formFieldsDateInput();
