4afd9c51f5174c18fa2fb2be57ccd6a93f63497b
rabbykhairul <rabbykhairul@gmail.com>
2023-12-10 22:49:03 +0600

    add cors header to all request

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { validateBasicAuth } from "./utils/auth";
 
+const protectedRoutePattern = /api\/app.*/gim;
 
 // This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
@@ -17,7 +18,22 @@ export function middleware(req: NextRequest) {
     });
   }
 
-  if (validateBasicAuth(req.headers)) {
+  let res: NextResponse;
+  if (!req.url.match(protectedRoutePattern)) {
+    res = NextResponse.next();
+    res.headers.append("Access-Control-Allow-Credentials", "true");
+    res.headers.append("Access-Control-Allow-Origin", "*");
+    res.headers.append(
+      "Access-Control-Allow-Methods",
+      "GET,DELETE,PATCH,POST,PUT",
+    );
+    res.headers.append(
+      "Access-Control-Allow-Headers",
+      "Authorization, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+    );
+
+    return res;
+  } else if (validateBasicAuth(req.headers)) {
     const res = NextResponse.next();
 
     res.headers.append("Access-Control-Allow-Credentials", "true");
@@ -42,6 +58,6 @@ export function middleware(req: NextRequest) {
 }
 
 // See "Matching Paths" below to learn more
-export const config = {
-  matcher: "/(api/app.*)",
-};
+// export const config = {
+//   matcher: "/(api/app.*)",
+// };
