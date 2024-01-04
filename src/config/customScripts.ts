7872c2a56d69e5a26e62686b098b80d00e45ca17
rabbykhairul <rabbykhairul@gmail.com>
2024-01-04 21:27:18 +0600

    update script version to 4.3.4

--- src/config/customScripts.ts
+++ src/config/customScripts.ts
@@ -62,13 +62,13 @@ export const CUSTOM_SCRIPTS_CONFIG: {
   // },
   [CUSTOM_SCRIPTS_NAME.FORM_FIELDS_PRO_CDN_SCRIPT]: {
     displayName: CUSTOM_SCRIPTS_NAME.FORM_FIELDS_PRO_CDN_SCRIPT,
-    version: "4.3.3",
+    version: "4.3.4",
     location: "footer",
     hosted: true,
     hostedLocation:
-      "https://cdn.jsdelivr.net/gh/rabbykhairul/jsdeliver-cdn-scripts@v4.3.3/form-fields-cdn.js",
+      "https://cdn.jsdelivr.net/gh/rabbykhairul/jsdeliver-cdn-scripts@v4.3.4/form-fields-cdn.js",
     integrityHash:
-      "sha384-Uqal6eik12dRnX2ozpWlki0HAByZteeQ4+V+xY+WcHQRHkRjwNHEciqHM6SC2ZNV",
+      "sha384-D6ZgookrYJ/3/phP89L/Tp4MjNXtBDaYFuQTVFbOGvgDfON6T/1mj7EWBjN+Gke2",
   },
   [CUSTOM_SCRIPTS_NAME.SENTRY_LOG_SCRIPT]: {
     displayName: CUSTOM_SCRIPTS_NAME.SENTRY_LOG_SCRIPT,
