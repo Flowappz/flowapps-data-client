02416ed78e505f4555f0b0d14b600dd4bc5ab4f7
rabbykhairul <rabbykhairul@gmail.com>
2024-01-16 21:41:40 +0600

    remove dead code

--- src/config/customScripts.ts
+++ src/config/customScripts.ts
@@ -11,11 +11,6 @@ export type ScriptConfig = {
 export const CUSTOM_SCRIPTS_BASE_FILE_PATH = "/src/server/webflow/scripts";
 
 export const CUSTOM_SCRIPTS_NAME = {
-  // DROPDOWN: "dropdown",
-  // DROPDOWN_SHOW_HIDE: "dropdown show hide",
-  // DATE_PICKER_SCRIPT: "date picker script",
-  // USER_IP_SCRIPT: "user ip script",
-  // NUMBER_SLIDER_SCRIPT: "number slider script",
   FORM_FIELDS_PRO_CDN_SCRIPT: "Form Fields Pro CDN Script",
   SENTRY_LOG_SCRIPT: "Sentry log script",
 } as const;
@@ -26,40 +21,6 @@ export type customScriptName = (typeof CUSTOM_SCRIPTS_NAME)[scriptKeys];
 export const CUSTOM_SCRIPTS_CONFIG: {
   [key in customScriptName]: ScriptConfig;
 } = {
-  // [CUSTOM_SCRIPTS_NAME.DROPDOWN]: {
-  //   displayName: CUSTOM_SCRIPTS_NAME.DROPDOWN,
-  //   version: "0.0.58",
-  //   location: "footer",
-  //   path: `${CUSTOM_SCRIPTS_BASE_FILE_PATH}/minified/dropdown.min.js`,
-  // },
-
-  // [CUSTOM_SCRIPTS_NAME.DROPDOWN_SHOW_HIDE]: {
-  //   displayName: CUSTOM_SCRIPTS_NAME.DROPDOWN_SHOW_HIDE,
-  //   version: "0.0.15",
-  //   location: "footer",
-  //   path: `${CUSTOM_SCRIPTS_BASE_FILE_PATH}/minified/dropdown-show-hide.min.js`,
-  // },
-
-  // [CUSTOM_SCRIPTS_NAME.DATE_PICKER_SCRIPT]: {
-  //   displayName: CUSTOM_SCRIPTS_NAME.DATE_PICKER_SCRIPT,
-  //   version: "0.0.7",
-  //   location: "footer",
-  //   path: `${CUSTOM_SCRIPTS_BASE_FILE_PATH}/minified/date-picker.min.js`,
-  // },
-
-  // [CUSTOM_SCRIPTS_NAME.USER_IP_SCRIPT]: {
-  //   displayName: CUSTOM_SCRIPTS_NAME.USER_IP_SCRIPT,
-  //   version: "0.0.0",
-  //   location: "footer",
-  //   path: `${CUSTOM_SCRIPTS_BASE_FILE_PATH}/minified/user-ip.min.js`,
-  // },
-
-  // [CUSTOM_SCRIPTS_NAME.NUMBER_SLIDER_SCRIPT]: {
-  //   displayName: CUSTOM_SCRIPTS_NAME.NUMBER_SLIDER_SCRIPT,
-  //   version: "0.0.2",
-  //   location: "footer",
-  //   path: `${CUSTOM_SCRIPTS_BASE_FILE_PATH}/minified/range-slider.min.js`,
-  // },
   [CUSTOM_SCRIPTS_NAME.FORM_FIELDS_PRO_CDN_SCRIPT]: {
     displayName: CUSTOM_SCRIPTS_NAME.FORM_FIELDS_PRO_CDN_SCRIPT,
     version: "4.3.5",
